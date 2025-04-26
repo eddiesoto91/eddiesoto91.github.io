9309c051ede9074ce451958c3607ee9b3d3b40fb
Eddie Soto <135383724+eddiesoto91@users.noreply.github.com>
2025-04-26 02:52:34 +0000

    react-tailwind

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <h1> Eddie Soto
+      </h1>
+      <p className="text-center text-gray-600 mt-4">
+        Data Analyst | Information Management | BI Developer
+      </p>
+    </div>
+  )
+}
+
+export default App
